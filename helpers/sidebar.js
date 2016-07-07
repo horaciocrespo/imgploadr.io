05335b142e153fde8cb0060d3bcaa96d04a102e9
jhonny Horacio <jhonny.h.cr@gmail.com>
2016-07-06 23:20:57 -0400

    added sidebar feature

--- helpers/sidebar.js
+++ helpers/sidebar.js
@@ -1,13 +1,29 @@
-var Stats = require('./stats'),
-	Images = require('./images'),
-	Comments = require('./comments');
+var Stats = require('./stats');
+var Images = require('./images');
+var Comments = require('./comments');
+var async = require('async');
 
 module.exports = function(viewModel, callback){
-	viewModel.sidebar = {
-		stats: Stats(),
-		popular: Images.popular(),
-		comments: Comments.newest()
-	};
-		
-	callback(viewModel);
+	async.parallel(
+    [
+      function(next) {
+        next(null, Stats());
+      },
+      function(next) {
+        next(null, Images.popular());
+      },
+      function(next) {
+        Comments.newest(next);
+      }
+    ], 
+
+    function(err, results){
+      viewModel.sidebar = {
+        stats: results[0],
+        popular: results[1],
+        comments: results[2]
+      };
+
+    callback(viewModel);
+  });
 };
